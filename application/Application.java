7259b6f2fb0122f7e78aeea4a89af1bf9ec273b1
lleandrroo <lleandrroo@hotmail.com>
2022-09-27 10:12:32 -0300

    final

--- application/Application.java
+++ application/Application.java
@@ -34,20 +34,20 @@ public class Application {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			// muiuto ruim - validação no programa principal
+			// muito ruim - validação no programa principal
 			Date now = new Date();
 			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
-			else if (!checkOut.after(checkIn)) {
 				System.out.println("Error in reservation: Check-out date must be after check-in date");
 			}
 			else {
 				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
-			}	
-		}
-		
+				
+				System.out.println();
+				System.out.println("Enter data to update the reservation: ");
+				System.out.println("Check-in date (dd/MM/yyyy): ");
+			}
+		}	
 		sc.close();
 	}
 }
